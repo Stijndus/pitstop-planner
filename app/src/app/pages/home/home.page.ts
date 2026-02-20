4041063e99c87cd70c4bdeb1b7a715f85358f9ec
Stijn Dusseldorp <stijn@ardusit.nl>
2026-02-20 23:14:30 +0100

    feat: refactor vehicle selection to use a button for vehicle picker and remove unused select component

--- app/src/app/pages/home/home.page.ts
+++ app/src/app/pages/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonSelect, IonSelectOption, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonButton, PickerController } from '@ionic/angular/standalone';
 import { VehicleService } from '../../services/vehicle.service';
 import { Vehicle } from '../../models/vehicle.model';
 
@@ -10,13 +10,16 @@ import { Vehicle } from '../../models/vehicle.model';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonSelect, IonSelectOption, IonItem, IonLabel, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonButton, CommonModule, FormsModule]
 })
 export class HomePage implements OnInit {
   vehicles: Vehicle[] = [];
   selectedVehicle: Vehicle | null = null;
 
-  constructor(private vehicleService: VehicleService) { }
+  constructor(
+    private vehicleService: VehicleService,
+    private pickerController: PickerController
+  ) { }
 
   ngOnInit() {
     this.loadVehicles();
@@ -33,9 +36,33 @@ export class HomePage implements OnInit {
     });
   }
 
-  onVehicleChange(event: any) {
-    const vehicleId = event.detail.value;
-    this.selectedVehicle = this.vehicles.find(v => v.id === vehicleId) || null;
+  async openVehiclePicker() {
+    const picker = await this.pickerController.create({
+      columns: [
+        {
+          name: 'vehicle',
+          options: this.vehicles.map(vehicle => ({
+            text: this.getVehicleDisplayName(vehicle),
+            value: vehicle.id
+          }))
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Confirm',
+          handler: (value) => {
+            const vehicleId = value.vehicle.value;
+            this.selectedVehicle = this.vehicles.find(v => v.id === vehicleId) || null;
+          }
+        }
+      ]
+    });
+
+    await picker.present();
   }
 
   getVehicleDisplayName(vehicle: Vehicle): string {
